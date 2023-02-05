807eba7d68b2a32af8e28d5a8ae38c23f4a744fc
Baurz <baurzhan.zh@gmail.com>
2023-02-04 19:47:47 -0500

    i

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
         <a className='navbar-brand' href='/'>
           <div className='d-flex'>
             <img src={logo} alt='logo' className='mr-2' />
-            <div>ProjectMgmt</div>
+            <div>Project Management @SDET School - Simple GraphQL App</div>
           </div>
         </a>
       </div>
